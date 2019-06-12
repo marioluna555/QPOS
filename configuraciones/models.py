e52ecc849bd0e8fe4a5c34c96e9f8bbe934a18ad
unknown <xyzalan3030@gmail.com>
2019-06-12 11:09:53 -0600

    Actualizacion 2019-06-12

--- configuraciones/models.py
+++ configuraciones/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-"""class bot_lin_paises(models.model):
+class bot_lin_pais(models.model):
     za_pais = models.intField(max_lenght=4, primary_key=true)
     nombre_pais = models.charField(max_lenght=25)
     abreviatura = models.charField(max_lenght=12)
     codigo_telefonico = models.charField(max_lenght=10)
     activo = models.booleanField()
 
-class bot_lin_departamentos(models.model):
+class bot_lin_departamento(models.model):
     za_departamento = models.intField(max_lenght=5, primary_key=true, unique=true)
     za_pais = models.intField(max_lenght=4, foreign_key=true, unique=true)
     nombre_departamento = models.charField(max_lenght=35)
     abreviatura = models.charField(max_lenght=12)
-    activo = models.booleanField()"""
+    activo = models.booleanField()
